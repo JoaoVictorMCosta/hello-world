9ed30a5e8e1132bc32d937f826b21f8b2341a596
João Victor M. Da Costa <108638735+JoaoVictorMCosta@users.noreply.github.com>
2026-05-09 14:19:26 +0000

    Exercicio de vetores concluido - negativos

---
+++ Product/src/vetores/negativos/negativos.java
@@ -0,0 +1,31 @@
+package vetores.negativos;
+
+import java.util.Scanner;
+
+public class negativos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+
+        int[] listaDeNumeros = new int[n];
+
+        for (int i = 0; i < listaDeNumeros.length; i++) {
+
+            System.out.print("Digite um numero: ");
+
+            listaDeNumeros[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Numeros Negativos: ");
+        for (int i = 0; i < listaDeNumeros.length; i++) {
+            if (listaDeNumeros[i] < 0) {
+                System.out.println(listaDeNumeros[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
